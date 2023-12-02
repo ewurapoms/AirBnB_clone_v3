b3042b2aa30e3335672d3f7058c55b264b3828c2
ewurapoms <mzpomaa35@gmail.com>
2023-12-02 03:28:44 -0800

    import and CORS edit on app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,14 +5,16 @@ blueprint registration and handles errors
 """
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 import os
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
